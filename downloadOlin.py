3bb4955c1672808968fbc1226256f5a1fc1872d2
wcdolphin <wcdolphin@gmail.com>
2013-04-11 16:54:35 -0400

    refactor exceptions and argument passing to be more easily used by others

--- downloadOlin.py
+++ downloadOlin.py
@@ -1,16 +1,10 @@
 import os
-from downloadMailmanArchives import main
-class Struct:
-    def __init__(self, **entries): 
-        self.__dict__.update(entries)
-
+from mailman_downloader import download
 if __name__ == '__main__':
-    args = {
-    'archive_root_url': ['https://lists.olin.edu/mailman/private/carpediem/', 'https://lists.olin.edu/mailman/private/helpme/'],
-    'password' : os.environ.get('ARCHIVE_LOGIN', "fail"),
-    'username' : os.environ.get('ARCHIVE_PASS', "fail"),
-    'force' : True,
-    'dest' : './archives'
-    }
-    main(Struct(**args))
 
+    download(['https://lists.olin.edu/mailman/private/carpediem/', 'https://lists.olin.edu/mailman/private/helpme/'],
+        password=os.environ.get('ARCHIVE_LOGIN', "fail"),
+        username=os.environ.get('ARCHIVE_PASS', "fail"),
+        force=True,
+        dest='./archives'
+    )
